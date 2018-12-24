5de7a12a77e396791bcba19cfd478f9c74384e54
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2018-12-23 23:16:40 -0500

    Component annotation is also fine on greeting repo

--- src/main/java/guru/springframework/services/GreetingRepositoryImpl.java
+++ src/main/java/guru/springframework/services/GreetingRepositoryImpl.java
@@ -1,13 +1,14 @@
 package guru.springframework.services;
 
 import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 /**
  * Created by jt on 5/24/17.
  */
-@Service
-@Primary
+//@Service
+@Component  // this also works
 public class GreetingRepositoryImpl implements GreetingRepository {
 
     @Override
