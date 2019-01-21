89509d00238846ec8eaf9499d2ae44e3cb6cf9d6
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2019-01-20 22:40:50 -0500

    using spring factory beans

--- src/main/java/guru/springframework/services/PrimaryGreetingService.java
+++ src/main/java/guru/springframework/services/PrimaryGreetingService.java
@@ -1,16 +1,10 @@
 package guru.springframework.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Service;
 
 /**
  * Created by jt on 5/24/17.
  */
-@Service
-@Primary
-@Profile({"en", "default"})
 public class PrimaryGreetingService implements GreetingService {
 
     private GreetingRepository greetingRepository;
--- src/main/java/guru/springframework/services/PrimarySpanishGreetingService.java
+++ src/main/java/guru/springframework/services/PrimarySpanishGreetingService.java
@@ -1,16 +1,10 @@
 package guru.springframework.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Service;
 
 /**
  * Created by jt on 5/24/17.
  */
-@Service
-@Profile("es")
-@Primary
 public class PrimarySpanishGreetingService implements GreetingService {
 
     private GreetingRepository greetingRepository;
